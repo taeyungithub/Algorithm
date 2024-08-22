e7563a09c2ccce2f2f9a5fefb0cba8508854e869
taeyun kim <144984315+taeyungithub@users.noreply.github.com>
2024-08-22 21:16:52 +0900

    [Bronze V] Title: 문자와 문자열, Time: 164 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class Main {
+	public static void main (String[] args) {
+		Scanner in = new Scanner(System.in);
+		String A = in.next();
+		int B = in.nextInt();
+       
+		System.out.print(A.charAt(B-1));
+	}
+}
