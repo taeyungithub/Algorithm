309e59c4595411c11ada092ce74af3350094d62b
taeyun kim <144984315+taeyungithub@users.noreply.github.com>
2024-08-25 19:36:13 +0900

    [Bronze I] Title: 이항 계수 1, Time: 176 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        long result = 0;
+
+        result = factorial(n) / (factorial(n - k) * factorial(k));
+
+        System.out.println(result);
+    }
+
+
+    public static long factorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
