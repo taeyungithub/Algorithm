2eb1907f8fc20c5f479df04d8dd31db1ae3a7c16
taeyun kim <144984315+taeyungithub@users.noreply.github.com>
2024-08-25 19:44:29 +0900

    [Bronze I] Title: FizzBuzz, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = 0;
+        boolean flag = false;
+
+        for (int i = 0; i < 3; i++) {
+            String str = br.readLine();
+
+            char c = str.charAt(0);
+
+            if (c != 'F' && c != 'B') {
+                num = Integer.parseInt(str);
+                flag = true;
+            }
+
+            if (flag) {
+                num++;
+            }
+        }
+
+        if (num % 3 == 0) {
+            if (num % 5 == 0) {
+                // FizzBuzz
+                System.out.println("FizzBuzz");
+            } else {
+                // Fizz
+                System.out.println("Fizz");
+            }
+        } else {
+            if (num % 5 == 0) {
+                // Buzz
+                System.out.println("Buzz");
+            } else {
+                // i
+                System.out.println(num);
+            }
+        }
+    }
+}
