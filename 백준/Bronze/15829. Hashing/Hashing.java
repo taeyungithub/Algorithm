822dd8acff286cf6cf676b5725c036a7f891e9dc
taeyun kim <144984315+taeyungithub@users.noreply.github.com>
2024-08-25 18:27:48 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14264 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,20 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		long result = 0;
+		long pow = 1;
+		for(int i = 0; i < n; i++) {
+			result += ((s.charAt(i) - 96) * pow);
+			pow = (pow * 31) % 1234567891;
+		}
+		System.out.println(result % 1234567891);
+	}
 
-        int n = sc.nextInt(); sc.nextLine();
-        String str = sc.nextLine();
-        char[] array = str.toCharArray();
-
-        long sum =0;
-        for (int i = 0; i < n; i++) {
-            int num = array[i] - 96;
-
-            sum += (num * Math.pow(31, i));
-
-        }
-        System.out.println(sum);
-    }
 }
