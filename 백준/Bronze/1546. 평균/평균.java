df1e8dd41a24d05329c053c41248f1c73b900d11
taeyun kim <144984315+taeyungithub@users.noreply.github.com>
2024-08-25 18:54:10 +0900

    [Bronze I] Title: 평균, Time: 216 ms, Memory: 18716 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double[] score = new double[n];
+        double max = 0;
+        for (int i = 0; i < n; i++) {
+            score[i] = sc.nextInt();
+            if (score[i] > max) {
+                max = score[i];
+            }
+        }
+        double sum =0;
+        for (int i = 0; i < n; i++) {
+            score[i] = score[i] / max * 100;
+            sum+=score[i];
+        }
+        double avg =  sum / n;
+        System.out.println(avg);
+    }
+}
