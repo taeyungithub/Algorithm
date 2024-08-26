7537c835880e4851ca95b3367fe459333dfb268b
taeyun kim <144984315+taeyungithub@users.noreply.github.com>
2024-08-26 19:38:04 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main { 
+	public static void main(String[] args){ 
+		Scanner s = new Scanner(System.in);
+		int A = s.nextInt();
+		int B = s.nextInt();
+		double C = (double)A / B;
+		
+		System.out.print(C);
+		
+		
+	}
+}
