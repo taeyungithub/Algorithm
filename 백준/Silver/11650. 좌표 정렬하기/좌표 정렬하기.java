dd9a0534421b8b4fa6cfffec799af0f71d6cd9e9
taeyun kim <144984315+taeyungithub@users.noreply.github.com>
2024-08-31 15:13:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1760 ms, Memory: 193444 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+
+    public static class coordinate {
+        int x;
+        int y;
+        int count;
+
+        coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return x + " " + y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        List<coordinate> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(new coordinate(sc.nextInt(), sc.nextInt()));
+        }
+
+        list.sort(new Comparator<coordinate>() {
+            @Override
+            public int compare(coordinate c1, coordinate c2) {
+                int result = c1.x - c2.x;
+                if (result == 0) {
+                    return c1.y - c2.y;
+                } else {
+                    return result;
+                }
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println(list.get(i));
+        }
+    }
+}
