811b65f44eefff0072fd08157ff34c88921a9e3e
taeyun kim <144984315+taeyungithub@users.noreply.github.com>
2024-09-03 08:50:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1120 ms, Memory: 183648 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			int input = Integer.parseInt(st.nextToken());
+            //해당 key값에 저장된 value가 null이면 1을 대입 아니면 +1을 해준다.
+			if(hash.get(input) == null) hash.put(input, 1);
+			else hash.put(input, hash.get(input) + 1);
+		}
+		
+		
+		int m = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < m; i++) {
+			int input = Integer.parseInt(st.nextToken());
+            //해당 key값에 저장된 value가 null이면 0을 sb에 저장해준다.
+			if(hash.get(input) == null) sb.append(0).append(" ");
+			else sb.append(hash.get(input)).append(" ");
+		}
+		System.out.println(sb);
+	}
+}
