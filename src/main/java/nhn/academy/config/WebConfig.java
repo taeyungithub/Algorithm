2571d7a33b402b77db1e245d933fdc0cdc48edd7
신건영/협업서비스개발레드팀/ND <kunyoung.shin@nhnent.com>
2024-09-24 09:36:34 +0900

    apply Content Negotiation

--- src/main/java/nhn/academy/config/WebConfig.java
+++ src/main/java/nhn/academy/config/WebConfig.java
@@ -1,7 +1,9 @@
 package nhn.academy.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,5 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         System.out.println(converters);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.parameterName("format")
+                .favorParameter(true).defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
     }
 }
